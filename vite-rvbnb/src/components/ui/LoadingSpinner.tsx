b84a63d8bc44508c808fca3aea93fea584da288d
Billy Romano <billy@romano3.com>
2024-12-12 12:32:44 -0500

    chore: removing old code

--- vite-rvbnb/src/components/ui/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const LoadingSpinner: React.FC = () => {
-  return (
-    <div className="absolute inset-0 flex items-center justify-center">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500" />
-    </div>
-  );
-};
